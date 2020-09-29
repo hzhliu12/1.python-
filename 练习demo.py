a43babf3a0fc8524a5b2047ec2815fc4172fe361
胡志辉 <70931768+hzhliu12@users.noreply.github.com>
2020-09-29 10:33:10 +0800

    Update 练习demo.py

--- 练习demo.py
+++ 练习demo.py
@@ -4,12 +4,14 @@ length = len(max(lines,key=len))
 lines = [line.rstrip().ljust(length,)+'#'+str(index)+'\n'# rstrip()剔除右边的空格，字符。 
          for index,line in enumerate(lines)]# enumerate 迭代器， index：行号  line：对应的内容
 ####for index,line in enumerate(open('Walden.txt','r')):
-#####    count += 1
+#####    count = 0 
 with open("makenew.txt",'w')as f1:#“a” 以“追加”模式打开
           #for line in lines:
+                  count+=1
            #    line_new = line.strip()#或使用  line_new = line.replace('\n', '') 替换删除
-            #   line_new = (line_new + r'#' + 'count'+'\n') #在末尾+ # +换行符
-             #  line_new = line_new.ljust(10)##
+            #  line_new = line_new.ljust()
+             #   line_new = (line_new + r'#' + count+'\n') #在末尾+ # +换行符
+         
          
             #如何再+行号呢？？？？？？？
          #print(line_new)
